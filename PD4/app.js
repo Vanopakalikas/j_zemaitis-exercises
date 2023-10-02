48897407b7b99ac8a215bfd38a1747513f9dc5e4
Programmer656 <piktapetelne@gmail.com>
2023-10-02 09:53:43 +0300

    Labai juokinga

---
+++ PD4/app.js
@@ -0,0 +1,11 @@
+let petName = "Suo";
+let petAge = "Senas";
+
+console.log(`${petName}, ${petAge}`);
+
+petName = "Kate";
+petAge = "Jauna";
+
+const buildingAdress = "Hogwarts School of Witchcraft and Wizardry, Hogwarts Castle. Scottish Highlands, United Kingdom";
+
+console.log(`${buildingAdress}`);
